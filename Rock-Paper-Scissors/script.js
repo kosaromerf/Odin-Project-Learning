a81d40f9098a0c904fdbbecde8db63b43fd5fa08
kosaromerf <kosaromerf@gmail.com>
2023-07-01 14:02:15 +0300

    Js added for button input

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -18,45 +18,32 @@ function getComputerChoice() {
     return computerGuess;
   }
 
-function getPlayerChoice() {
-    let playerGuess = prompt('Enter your choice: Rock, Paper, or Scissors');
-    playerGuess = playerGuess.toLowerCase().trim()
-    if (playerGuess === "rock" || playerGuess === "paper" || playerGuess === "scissors") {
-    return playerGuess;
-    } else {
-        alert("invalid input");
-        return  getPlayerChoice();
-    }
-}
-
-function oneGame(){
-    let b = getComputerChoice();
-    let a = getPlayerChoice();
-    if ( a === "rock"){
-        if(b === "paper"){
+function oneGame(playerChoice,computerChoice){
+    if ( playerChoice === "rock"){
+        if(computerChoice === "paper"){
             l++
             return console.log("You Lose. Paper beats Rock")                 
-        } else if( b === "rock"){
+        } else if( computerChoice === "rock"){
             return console.log("Its a tie")
         }else{
             w++
             return console.log("You Win. Rock beats Scissors")
         }
-    }else if (a === "paper"){
-        if(b === "scissors"){
+    }else if (playerChoice === "paper"){
+        if(computerChoice === "scissors"){
             l++
                 return console.log("You Lose. Scissors beats Paper")           
-        } else if( b === "paper"){
+        } else if( computerChoice === "paper"){
             return console.log("Its a tie")
         }else{
             w++
                 return console.log("You Win. Rock beats Scissors")
         }  
     } else {
-        if(b === "rock"){
+        if(computerChoice === "rock"){
             l++
         return console.log("You Lose. Rock beats Scissors")           
-        } else if( b === "scissors"){
+        } else if( computerChoice === "scissors"){
             return console.log("Its a tie")
         }else{
             w++
@@ -65,16 +52,48 @@ function oneGame(){
     }    
 }
 
+function updateResult(result) {
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = result;
+}
+
 function game(){
-    while (w < 5 && l < 5){
-        oneGame()
-        console.log("Your score : " + w + "- Computer Score: " + l)
-    }
-    if (w === 5){
-        console.log("Yeeeeeeeey you won the game")
-    }else{
-        console.log("Boooooooooo you lose the game")
-    }
+  const rock = document.getElementById("rock");
+  const paper = document.getElementById("paper");
+  const scissors = document.getElementById("scissors");
+
+  rock.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = oneGame("rock", computerChoice);
+    updateResult(`You chose Rock. ${result} Computer chose ${computerChoice}.`);
+    checkScore()
+  });
+
+  paper.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = oneGame("paper", computerChoice);
+    updateResult(`You chose Paper. ${result} Computer chose ${computerChoice}.`);
+    checkScore()
+  });
+
+  scissors.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = oneGame("scissors", computerChoice);
+    updateResult(`You chose Scissors. ${result} Computer chose ${computerChoice}.`);
+    checkScore()
+  });
+
+}
+
+function checkScore() {
+  if (w === 5) {
+    console.log("Yeeeeeeeey you won the game");
+  } else if (l === 5) {
+    console.log("Boooooooooo you lose the game");
+  } else {
+    console.log("Your score: " + w + " - Computer Score: " + l);
+  }
 }
 
 game()
+ 
