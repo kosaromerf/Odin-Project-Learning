e6b54f903ef12fcd17159028245f9eb136b181af
kosaromerf <kosaromerf@gmail.com>
2023-07-22 04:05:40 +0300

    Basic Project is over. Color buttons added. Css styling is added. Error Massages are added.

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,99 +1,72 @@
+function setBoxes(size) {
+  let container = document.querySelector("#container");
 
+  container.innerHTML = "";
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-  function setBoxes(size) {
-    let container = document.querySelector("#container");
-  
-    container.innerHTML = "";
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  
-    for (let i = 0; i < size ** 2; i++) {
-      const box = document.createElement("div");
-      box.classList.add("box");
-      container.appendChild(box);
-  
+  for (let i = 0; i < size ** 2; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+
+    const whiteButton = document.getElementById("white");
+    const blackButton = document.getElementById("black");
+    const rainbowButton = document.getElementById("rainbow");
+    const shadeButton = document.getElementById("shade");
+
+    whiteButton.addEventListener("click", () => {
       box.addEventListener("mouseover", () => {
-        box.classList.add("hovered");
-        
+        box.style.backgroundColor = "white";
       });
-
-
-
-
-    }
-  }
-  
-  function reset() {
-    const reset = document.querySelector(".reset");
-  
-    reset.addEventListener("click", () => {
-      const changedBoxes = document.querySelectorAll(".hovered");
-      changedBoxes.forEach((box) => box.classList.remove("hovered"));
     });
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const submit = document.querySelector(".submit");
-    const input = document.querySelector("#input");
-  
-    setBoxes(16);
-  
-    submit.addEventListener("click", () => {
-      const size = parseInt(input.value);
-      setBoxes(size);
-      
-    });
-  
-    reset();
-  });
-
-
-
-  
-  
-  function changeStyle(){
-    const blackButton =document.getElementById("black")
-    const whiteButton =document.getElementById("white")
-    const rainbowButton =document.getElementById("rainbow")
-    const shadeButton =document.getElementById("shade")
-    const hovered =document.querySelectorAll(".hovered")
 
     blackButton.addEventListener("click", () => {
-        const blackStyleElement = document.createElement("style");
-        const blackStyle =`
-        .hovered{
-            background-color: black
-        }`
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "black";
+      });
+    });
 
-        blackStyleElement.innerHTML = blackStyle;
-
-        document.head.appendChild(blackStyleElement);
-    })
-    
-    whiteButton.addEventListener("click", () => {
-        const whiteStyleElement = document.createElement("style");
-        const whiteStyle =`
-        .hovered{
-            background-color: white
-        }`
-
-        whiteStyleElement.innerHTML = whiteStyle;
-
-        document.head.appendChild(whiteStyleElement);
-    })
-
-        
+    function randomizeColor() {
+      let num = Math.floor(Math.random() * 255);
+      return num;
     }
 
+    rainbowButton.addEventListener("click", () => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = `rgb(${randomizeColor()},${randomizeColor()},${randomizeColor()})`;
+      });
+    });
 
+    let numPercent = 100;
 
-    changeStyle()
+    function shadeColor() {
+      numPercent -= 10;
+      return numPercent;
+    }
+    shadeButton.addEventListener("click", () => {
+      box.addEventListener("mouseover", () => {
+        let newColor = shadeColor();
+        box.style.backgroundColor = `rgb(${newColor}%,${newColor}%,${newColor}%)`;
+      });
+    });
+  }
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  const submit = document.querySelector(".submit");
+  const input = document.querySelector("#input");
 
+  setBoxes(16);
 
-
-
-
-
-
-
+  submit.addEventListener("click", () => {
+    const size = parseInt(input.value);
+    if (size <= 100 && size > 0) {
+      setBoxes(size);
+    } else if (size > 1) {
+      alert("You need to submit number lover than 101");
+    } else {
+      alert("You need to submit a positive interger");
+    }
+  });
+});
