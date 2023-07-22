2831d40b8b7050968907fff674c0bb080bcad02e
kosaromerf <kosaromerf@gmail.com>
2023-07-22 04:07:21 +0300

    Started with basic html.

---
+++ Calculator/script.js
@@ -0,0 +1,38 @@
+function addition (num1,num2) {
+    return num1+num2
+}
+
+function substraction (num1,num2) {
+    return num1-num2
+}
+
+function multiplication (num1,num2) {
+    return num1*num2
+}
+
+function divition (num1,num2) {
+    return num1/num2
+}
+
+
+
+let num1 = 
+let num2 =
+let operator =
+
+
+function operate(num1,operator,num2){
+    if(operator== "+"){
+        return addition(num1,num2)
+    }else if(operator== "-"){
+        return substraction(num1,num2)
+    }else if(operator== "X"){
+        return multiplication(num1,num2)
+    }else if(operator== "/"){
+        return divition(num1,num2)
+    }
+
+}
+
+
+let value = document.querySelector()
