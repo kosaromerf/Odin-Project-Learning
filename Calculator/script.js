e405066db44456fc89c109abdba2738cbf47ee57
kosaromerf <kosaromerf@gmail.com>
2023-07-30 21:33:54 +0300

    Functinol Calculator
    there are sum bugs to fix
    -point can entered multiple times thats a issue
    -looks ugly need css changes maybe after a framework

--- Calculator/script.js
+++ Calculator/script.js
@@ -16,71 +16,36 @@ function division(num1, num2) {
   return num1 / num2;
 }
 
-//Declearing variables for inputs
-
-let first = parseFloat(displayInput);
-let second = parseFloat(displayInput);
-let operator = parseFloat(getOperator);
 //operate function
 
 function operate(first, operator, second) {
   switch (operator) {
     case "+":
-      result = addition(first, second);
-      break;
+      return addition(first, second);
     case "-":
-      result = substraction(first, second);
-      break;
+      return substraction(first, second);
+
     case "*":
-      result = multiplication(first, second);
-      break;
+      return multiplication(first, second);
+
     case "/":
-      result = division(first, second);
-      break;
+      if (second === 0) return alert("You can't divide by 0!");
+      else return division(first, second);
   }
-  return result;
 }
-
-/*equal.addEventListener("click", () => {
-  const expression = display.textContent;
-  const [num1, op, num2] = expression.split(/(\+|\-|\*|\/)/);
-  const first = parseFloat(num1);
-  const second = parseFloat(num2);
-
-  let result;
-  switch (op) {
-    case "+":
-      result = addition(first, second);
-      break;
-    case "-":
-      result = substraction(first, second);
-      break;
-    case "*":
-      result = multiplication(first, second);
-      break;
-    case "/":
-      if (second === 0) {
-        result = "Error: Division by zero";
-      } else {
-        result = division(first, second);
-      }
-      break;
-    default:
-      result = "Error: Invalid operator";
-  }
-
-  display.textContent = result;
-});*/
 
 // Display functions
 
-const buttons = document.querySelectorAll(".display");
+const numbers = document.querySelectorAll(".number");
 const display = document.getElementById("display");
 const equal = document.getElementById("equal");
 const reset = document.getElementById("reset");
+const backspace = document.getElementById("backspace");
+const sing = document.getElementById("sing");
+const oper = document.querySelectorAll(".operator");
 
 function displayInput() {
-  buttons.forEach((number) => {
+  numbers.forEach((number) => {
     number.addEventListener("click", () => {
       const numberText = number.textContent;
       display.textContent += numberText;
@@ -90,5 +55,121 @@ function displayInput() {
 
 displayInput();
 
-function getOperator() {}
-getOperator();
+function roundDecimal(result) {
+  return Math.round(result * 100000) / 100000;
+}
+
+oper.forEach((op) => {
+  op.addEventListener("click", () => {
+    try {
+      const first = parseFloat(getNumber1());
+      const second = parseFloat(getNumber2());
+      const operator = getOperator();
+      const result = operate(first, operator, second);
+      const roundedResult = roundDecimal(result);
+      display.textContent = roundedResult.toString() + op.textContent;
+    } catch (error) {
+      const numberText = op.textContent;
+      display.textContent += numberText;
+    }
+  });
+});
+
+equal.addEventListener("click", () => {
+  try {
+    const first = parseFloat(getNumber1());
+    const second = parseFloat(getNumber2());
+    const operator = getOperator();
+    const result = operate(first, operator, second);
+    const roundedResult = roundDecimal(result);
+    display.textContent = roundedResult.toString();
+  } catch (error) {
+    alert("No valid input");
+  }
+});
+
+reset.addEventListener("click", () => {
+  display.textContent = "";
+});
+
+backspace.addEventListener("click", () => {
+  display.textContent = display.textContent.toString().slice(0, -1);
+});
+
+sing.addEventListener("click", () => {
+  display.textContent = display.textContent * -1;
+});
+
+// Dividing Display
+
+function getNumber1() {
+  regex1 = /^[-]?\d+[.]?\d*/;
+  return display.textContent.match(regex1)[0];
+}
+
+function getNumber2() {
+  regex2 = /\d+[.]?\d*$/;
+  return display.textContent.match(regex2)[0];
+}
+
+function getOperator() {
+  regex3 = /\d[+\-*/]/;
+  return display.textContent.match(regex3)[0].slice(1);
+}
+
+// Keyboard Support
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  const validKeys = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    ".",
+    "=",
+    "Enter",
+    "Escape",
+    "Backspace",
+  ];
+
+  if (validKeys.includes(key)) {
+    event.preventDefault();
+
+    if (key === "Enter" || key === "=") {
+      const first = parseFloat(getNumber1());
+      const second = parseFloat(getNumber2());
+      const operator = getOperator();
+      const result = operate(first, operator, second);
+      const roundedResult = roundDecimal(result);
+      display.textContent = roundedResult.toString();
+    } else if (key === "Escape") {
+      display.textContent = "";
+    } else if (key === "Backspace") {
+      display.textContent = display.textContent.toString().slice(0, -1);
+    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+      try {
+        const first = parseFloat(getNumber1());
+        const second = parseFloat(getNumber2());
+        const operator = getOperator();
+        const result = operate(first, operator, second);
+        const roundedResult = roundDecimal(result);
+        display.textContent = roundedResult.toString() + key;
+      } catch (error) {
+        const numberText = key;
+        display.textContent += numberText;
+      }
+    } else {
+      display.textContent += key;
+    }
+  }
+});
